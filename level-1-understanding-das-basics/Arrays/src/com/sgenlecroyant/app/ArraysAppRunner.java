def80a15307c3ec22d4f9abe487c162f118c6a53
sgenlecroyant <sgenlecroyant@gmail.com>
2024-05-01 11:04:10 +0300

    feat(arrays): init the arrays module and add .gitignore file

---
+++ level-1-understanding-das-basics/Arrays/src/com/sgenlecroyant/app/ArraysAppRunner.java
@@ -0,0 +1,8 @@
+package com.sgenlecroyant.app;
+
+public class ArraysAppRunner {
+
+    public static void main(String[] args) {
+        System.out.println("The journey starts from here");
+    }
+}
