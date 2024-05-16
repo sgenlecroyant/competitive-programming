5e66462676fd201bc5bacbb46769dc4c7f0798c2
sgenlecroyant <sgenlecroyant@gmail.com>
2024-05-16 23:30:31 +0300

    feat(linkedlists): init project

---
+++ level-1-understanding-das-basics/linkedlists/src/main/java/com/sgenlecroyant/app/linkedlists/LinkedlistsApplication.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.app.linkedlists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedlistsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkedlistsApplication.class, args);
+    }
+
+}
---
+++ level-1-understanding-das-basics/linkedlists/src/test/java/com/sgenlecroyant/app/linkedlists/LinkedlistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.app.linkedlists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedlistsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
