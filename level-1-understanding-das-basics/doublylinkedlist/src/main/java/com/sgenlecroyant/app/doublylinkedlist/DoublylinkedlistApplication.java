6485396f77c69683be1c5152e5e4763ef293dfed
sgenlecroyant <sgenlecroyant@gmail.com>
2024-05-23 22:33:04 +0300

    create doubly-linked-list project

---
+++ level-1-understanding-das-basics/doublylinkedlist/src/main/java/com/sgenlecroyant/app/doublylinkedlist/DoublylinkedlistApplication.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.app.doublylinkedlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoublylinkedlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoublylinkedlistApplication.class, args);
+	}
+
+}
---
+++ level-1-understanding-das-basics/doublylinkedlist/src/test/java/com/sgenlecroyant/app/doublylinkedlist/DoublylinkedlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.app.doublylinkedlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoublylinkedlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
