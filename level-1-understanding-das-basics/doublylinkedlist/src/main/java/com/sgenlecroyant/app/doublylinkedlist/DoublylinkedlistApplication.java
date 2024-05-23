a589b03588feb28c917b9aea0acf860c2839b363
sgenlecroyant <sgenlecroyant@gmail.com>
2024-05-23 22:41:27 +0300

    feat(doublylinkedlist): removed spring-boot app stuffs

--- level-1-understanding-das-basics/doublylinkedlist/src/main/java/com/sgenlecroyant/app/doublylinkedlist/DoublylinkedlistApplication.java
+++ level-1-understanding-das-basics/doublylinkedlist/src/main/java/com/sgenlecroyant/app/doublylinkedlist/DoublylinkedlistApplication.java
@@ -1,13 +1,9 @@
 package com.sgenlecroyant.app.doublylinkedlist;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DoublylinkedlistApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoublylinkedlistApplication.class, args);
+
 	}
 
 }
