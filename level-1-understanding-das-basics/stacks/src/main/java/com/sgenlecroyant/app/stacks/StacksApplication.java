60104783d98993eae59ca6dc7ccb9b91c33d893e
sgenlecroyant <sgenlecroyant@gmail.com>
2024-05-26 19:25:05 +0300

    format the codebase

--- level-1-understanding-das-basics/stacks/src/main/java/com/sgenlecroyant/app/stacks/StacksApplication.java
+++ level-1-understanding-das-basics/stacks/src/main/java/com/sgenlecroyant/app/stacks/StacksApplication.java
@@ -1,17 +1,8 @@
 package com.sgenlecroyant.app.stacks;
 
-import java.io.IOException;
-
 public class StacksApplication {
 
-	public static void main(String[] args) throws IOException {
-
-		AppStack<String> appStack = new AppStack<>("google");
-		appStack.push("udemy");
-		appStack.push("discord");
-
-		System.out.println(appStack.peek());
+	public static void main(String[] args) {
 
 	}
-
 }
